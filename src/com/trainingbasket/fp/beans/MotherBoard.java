a4943bbff1f9c5715573c152ea84d2ba950d9807
pooja <poojasep1998@gmail.com>
2020-02-23 13:42:14 +0530

    modified RAM and Motherboard

--- src/com/trainingbasket/fp/beans/MotherBoard.java
+++ src/com/trainingbasket/fp/beans/MotherBoard.java
@@ -2,17 +2,46 @@ package com.trainingbasket.fp.beans;
 
 public class MotherBoard {
 
-    public MotherBoard(String company) {
-        this.company = company;
+   String overview;
+   int cost;
+   String motherboardId;
+
+    public MotherBoard(String overview, int cost, String motherboardId) {
+        this.overview = overview;
+        this.cost = cost;
+        this.motherboardId = motherboardId;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getMotherboardId() {
+        return motherboardId;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setMotherboardId(String motherboardId) {
+        this.motherboardId = motherboardId;
     }
 
     @Override
     public String toString() {
         return "MotherBoard{" +
-                "company='" + company + '\'' +
+                "overview='" + overview + '\'' +
+                ", cost=" + cost +
+                ", motherboardId='" + motherboardId + '\'' +
                 '}';
     }
-
-    String company;
-
 }
